e4ced96b20bb76b978ac591c0708b23d40970f17
shishir49 <ks.azim@yahoo.com>
2021-04-12 07:54:58 +0600

    Update : Frontend and PaymentGateways

--- app/Models/Transection.php
+++ app/Models/Transection.php
@@ -9,7 +9,8 @@ class Transection extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['method_id','member_id','transection_type','name','email','phone','address','amount','charge','campaign_author','campaign_id','status'];
+
+protected $fillable = ['method_id','member_id','transection_type','name','email','phone','address','amount','charge','campaign_author','campaign_id','status'];
 
     public function Members(){
         return $this->belongsTo('App\Models\User','member_id');
